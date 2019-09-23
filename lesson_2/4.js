972dbedf4c195e17f35190db4da3352f38460c68
PTaras <potsikaylot@gmail.com>
2019-09-23 19:55:13 +0300

    lesson_4_1

--- lesson_2/4.js
+++ lesson_2/4.js
@@ -2,14 +2,15 @@ const value = 0;
 
 switch (value) {
   case 0:
-  console.log(0);
-  break;
+    console.log(0);
+    break;
 
   case 1:
-  console.log(1);
-  break;
+    console.log(1);
+    break;
 
-  case 2 || 3:
-  console.log('2,3');
-  break;
+  case 2:
+  case 3:
+    console.log('2,3');
+    break;
 }
