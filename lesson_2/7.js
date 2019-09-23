71eeb173fb098d8416e6725324a4f1a7a3d4ac33
PTaras <potsikaylot@gmail.com>
2019-09-23 20:06:26 +0300

    lesson_7_1

--- lesson_2/7.js
+++ lesson_2/7.js
@@ -3,7 +3,9 @@ const array = [1, 2, 3, 4];
 var sum = 0;
 
 for (var i = 0; i < array.length; i++) {
-  sum += array[i % 2];
+  if (array[i] % 2 == 0) {
+      sum += array[i];
+  }
 }
 
 console.log(sum);
