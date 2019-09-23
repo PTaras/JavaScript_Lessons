84b626de15edc67e066330afdd14f7480512bc47
PTaras <potsikaylot@gmail.com>
2019-09-23 19:51:35 +0300

    lesson_3_1

--- lesson_2/3.js
+++ lesson_2/3.js
@@ -1,12 +1,11 @@
 const value = 'c';
 
-if (value == 'a') {
+if (value === 'a') {
   console.log('a');
-} else if (value == 'b') {
-} else if (value == 'c') {
-  console.log('others');
-} else if (value == 'd') {
-} else if (value == 'e') {
 } else {
-  console.log('unknown');
+  if (value === 'b' || 'c' || 'd' || 'e') {
+    console.log('others');
+  } else {
+    console.log('unknown');
+  }
 }
