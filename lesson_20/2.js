1af1ab8c7441b71993ac424cc5aa7afa8a224023
PTaras <potsikaylot@gmail.com>
2019-11-27 20:16:00 +0200

    lesson_20

---
+++ lesson_20/2.js
@@ -0,0 +1,50 @@
+/** 
+# Задача 2
+
+Улучшите класс `Customers` добавив функцию генератор.
+
+**Обратите внимание**:
+
+1. Класс `Customers` после этого должен работать **идентично** предыдущей задаче.
+*/
+
+// Решение
+class Customers {
+    constructor() {
+        this.customers = [];
+    }
+    
+    add(customer) {
+        if (customer.name !== 'undefined' && customer.verified === true) {
+            this.customers.push(customer)
+        }
+    }
+
+    *[Symbol.iterator]() {
+        for (const customer of this.customers) {
+            yield customer;
+        }
+    }
+}
+
+// Пример использования
+const customers = new Customers();
+customers.add({name: 'Alex'});
+customers.add({name: 'Victor'});
+customers.add({name: 'Marcus'});
+customers.add({name: 'Andrii', verified: true});
+customers.add({name: 'Marco', verified: true});
+customers.add({name: 'Oliver'});
+customers.add({name: 'Lisa', verified: true});
+customers.add({name: 'John'});
+customers.add({name: 'Ivan', verified: true});
+
+for (const customer of customers) {
+    console.log(customer);
+}
+
+// В консоли будет
+// { name: 'Andrii', verified: true }
+// { name: 'Marco', verified: true }
+// { name: 'Lisa', verified: true }
+// { name: 'Ivan', verified: true }
